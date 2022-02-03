025c77f9aca18d770da27cf48a57d2d13421c9f0
KAMONPAN KAEWHEANG <kamonpan@dit.daikin.co.jp>
2022-02-03 15:30:59 +0700

    Initial Commit

---
+++ Hobbies Spender/Hobbies_SpenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hobbies_SpenderApp.swift
+//  Hobbies Spender
+//
+//  Created by admin on 3/2/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct Hobbies_SpenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
